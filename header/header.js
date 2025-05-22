c77b7ee89a2588394e11420f8c7ab715862d3912
Genaro <genaroduca10@gmail.com>
2025-05-22 19:46:40 -0300

    home: new menu options and responsive fixed. studio: left-menu, right-menu fixed position new diagram update remove innecesary comments

--- header/header.js
+++ header/header.js
@@ -34,3 +34,11 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+
+
+const sidebar = document.querySelector('.rsm-sidebar');
+const sidebarToggler = document.querySelector('.rsm-sidebar-toggler');
+
+sidebarToggler.addEventListener('click', () => {
+    sidebar.classList.toggle('collapsed');
+});
