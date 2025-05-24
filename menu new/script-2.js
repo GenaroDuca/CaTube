07c5350a918f055f40034a869a900edd52472486
Jeremias Nehuen Mastroianni <jeremiasmastroianni00@gmail.com>
2025-05-24 00:30:14 -0300

    Channel Analytics terminado

--- menu new/script-2.js
+++
@@ -1,6 +0,0 @@
-const sidebar = document.querySelector('.rsm-sidebar');
-const sidebarToggler = document.querySelector('.rsm-sidebar-toggler');
-
-sidebarToggler.addEventListener('click', () => {
-  sidebar.classList.toggle('collapsed');
-});
