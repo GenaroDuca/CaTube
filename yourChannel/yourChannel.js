a96cb5b9d01cca0eb8b175c637ffcf8c89f30ff6
AngelNovelli <angeljesusnovelli@gmail.com>
2025-05-24 21:41:39 -0300

    yourChannel finished

--- yourChannel/yourChannel.js
+++ yourChannel/yourChannel.js
@@ -1,6 +1,22 @@
+// Carrusel
 function scrollCarousel(direction, containerId) {
   const container = document.getElementById(containerId);
-  const scrollAmount = 400; // puede ajustarse a 390 o más
+  let scrollAmount;
+
+  switch (containerId) {
+    case 'carousel-foryou':
+      scrollAmount = 400;
+      break;
+    case 'videos-container':
+      scrollAmount = 325;
+      break;
+    case 'popular-container':
+      scrollAmount = 325;
+      break;
+    case 'carousel-shorts':
+      scrollAmount = 270;
+      break; 
+  }
 
   if (direction === 'left') {
     container.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
@@ -8,3 +24,31 @@ function scrollCarousel(direction, containerId) {
     container.scrollBy({ left: scrollAmount, behavior: 'smooth' });
   }
 }
+
+//Hide and show sections
+ function setupTabNavigation(buttonSelector, sectionSelector) {
+  const buttons = document.querySelectorAll(buttonSelector);
+  const sections = document.querySelectorAll(sectionSelector);
+
+  buttons.forEach(button => {
+    button.addEventListener("click", function () {
+      buttons.forEach(btn => btn.classList.remove("active"));
+      sections.forEach(section => section.classList.add("hide"));
+
+      this.classList.add("active");
+
+      const sectionId = this.getAttribute("data-section");
+      const activeSection = document.getElementById(sectionId);
+      if (activeSection) {
+        activeSection.classList.remove("hide");
+      }
+    });
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  setupTabNavigation(".nav-btn", ".content-table");
+  setupTabNavigation(".nav-btn-videos", ".content-table-videos");
+  setupTabNavigation(".nav-btn-shorts", ".content-table-shorts");
+  setupTabNavigation(".nav-btn-posts", ".content-table-posts");
+});
