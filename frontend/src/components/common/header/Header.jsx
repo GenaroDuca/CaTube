235f0291bd8a7dbc59c1b6644fba273ab80cc9e7
Jeremias Nehuen Mastroianni <jeremiasmastroianni00@gmail.com>
2025-11-06 12:29:59 -0300

    modoClaro listo

--- frontend/src/components/common/header/Header.jsx
+++ frontend/src/components/common/header/Header.jsx
@@ -1,11 +1,14 @@
+import { useTheme } from '../../context/themeContext.jsx'
 import logo from '../../../../public/catube_white.svg'
+import logoDark from '../../../../public/catube_dark.svg'
 import { CatubeHeader } from './CatubeHeader.jsx'
 import './CatubeHeader.css'
 
 function Header( { searchQuery, setSearchQuery}) {
+    const { isDarkMode } = useTheme();
     return (
         <CatubeHeader
-        logo={logo}
+        logo={isDarkMode ? logo : logoDark}
         searchQuery={searchQuery}
         setSearchQuery={setSearchQuery}
         />
