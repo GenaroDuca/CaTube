eb9f270cbfb0d2765021d78728dbaf75b7ddd617
Genaro <genaroduca10@gmail.com>
2025-11-20 08:56:41 -0300

    cambio revertido

--- frontend/src/components/homePageComponents/Profile.jsx
+++ frontend/src/components/homePageComponents/Profile.jsx
@@ -4,20 +4,20 @@ function Profile({ namechannel, subschannel, thumbnail, url }) {
     const firstLetter = namechannel?.charAt(0).toUpperCase();
 
     // Determinar la URL final de la foto
-    // let photoSrc = props.thumbnail && props.thumbnail.startsWith('http')
-    //     ? props.thumbnail
-    //     : VITE_API_URL + props.thumbnail;
+    let photoSrc = props.thumbnail && props.thumbnail.startsWith('http')
+        ? props.thumbnail
+        : VITE_API_URL + props.thumbnail;
         
-    // if (!photoSrc) {
-    //     // Por defecto, mostrar imagen según la primera letra
-    //     photoSrc = `/assets/images/profile/${firstLetter}.png`;
-    // }
+    if (!photoSrc) {
+        // Por defecto, mostrar imagen según la primera letra
+        photoSrc = `/assets/images/profile/${firstLetter}.png`;
+    }
 
 
     return (
         <Link to={`/yourchannel/${url}`}>
             <div className="profile">
-                <img className="profile-photo" src={props.thumbnail} alt={namechannel} />
+                <img className="profile-photo" src={photoSrc} alt={namechannel} />
                 <p className="name-channel">{namechannel}</p>
                 <p className="subs-channel">{subschannel} Cats</p>
             </div>
