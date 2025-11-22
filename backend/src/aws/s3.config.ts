80382cfbb202ac3a04e9174c93daacb0b3606a97
Genaro <genaroduca10@gmail.com>
2025-11-22 04:44:49 -0300

    correcion error no dejaba subir videos con s3

--- backend/src/aws/s3.config.ts
+++ backend/src/aws/s3.config.ts
@@ -1,9 +1,22 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
-export const s3 = new S3Client({
-  region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-  },
-});
+export const getS3Client = () => {
+    
+    const accessKeyId = process.env.AWS_ACCESS_KEY_ID?.trim();
+    const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY?.trim();
+    const region = process.env.AWS_REGION?.trim();
+
+    // Verificación de seguridad
+    if (!accessKeyId || !secretAccessKey || !region) {
+        console.error("❌ ERROR S3: Credenciales de AWS faltantes o inválidas.");
+        throw new Error("AWS credentials missing. Ensure .env is loaded first.");
+    }
+
+    return new S3Client({
+        region: region,
+        credentials: {
+            accessKeyId: accessKeyId,
+            secretAccessKey: secretAccessKey,
+        },
+    });
+};
