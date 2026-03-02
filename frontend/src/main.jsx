9ae1e82a424052f763b2a71cb439b401250d7995
Genaro Duca <genaroduca10@gmail.com>
2026-03-02 12:45:21 -0300

    Componente <Analytics /> de vercel

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,38 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Páginas
-import Home from './pages/HomePage/HomePage.jsx'
-import Trending from './pages/TrendingPage/TrendingPage.jsx'
-import Catscribers from './pages/CatscribersPage/CatscribersPage.jsx'
-import Education from './pages/EducationPage/EducationPage.jsx'
-import You from './pages/YouPage/YouPage.jsx'
-import Studio from './pages/StudioPage/StudioPage.jsx'
-import YourChannel from './pages/YourChannelPage/YourChannelPage.jsx'
-import DiscoverPage from './pages/DiscoverPage/DiscoverPage.jsx'
-import RegisterPage from './pages/RegisterPage/RegisterPage.jsx'
-import ShortsPage from './pages/ShortPage/ShortsPage.jsx'
-import { VideoPage } from './pages/VideoPage/VideoPage.jsx'
-import SuccessPage from "./pages/VerificationEmailPage/SuccessPage.jsx"
-import ErrorPage from './pages/VerificationEmailPage/ErrorPage.jsx';
-import { Search } from './pages/SearchPage/SearchPage.jsx'
-import { ResetPasswordPage } from './pages/ResetPasswordPage/ResetPasswordPage.jsx';
+import Home from "./pages/HomePage/HomePage.jsx";
+import Trending from "./pages/TrendingPage/TrendingPage.jsx";
+import Catscribers from "./pages/CatscribersPage/CatscribersPage.jsx";
+import Education from "./pages/EducationPage/EducationPage.jsx";
+import You from "./pages/YouPage/YouPage.jsx";
+import Studio from "./pages/StudioPage/StudioPage.jsx";
+import YourChannel from "./pages/YourChannelPage/YourChannelPage.jsx";
+import DiscoverPage from "./pages/DiscoverPage/DiscoverPage.jsx";
+import RegisterPage from "./pages/RegisterPage/RegisterPage.jsx";
+import ShortsPage from "./pages/ShortPage/ShortsPage.jsx";
+import { VideoPage } from "./pages/VideoPage/VideoPage.jsx";
+import SuccessPage from "./pages/VerificationEmailPage/SuccessPage.jsx";
+import ErrorPage from "./pages/VerificationEmailPage/ErrorPage.jsx";
+import { Search } from "./pages/SearchPage/SearchPage.jsx";
+import { ResetPasswordPage } from "./pages/ResetPasswordPage/ResetPasswordPage.jsx";
 
 // Providers y Componentes Globales
-import { ModalProvider } from './components/common/modal/ModalContext.jsx'
-import ModalRenderer from './components/common/modal/ModalRenderer.jsx'
-import { SidebarProvider } from './hooks/useSidebarToggle.jsx';
-import { FriendMenu } from './components/common/friendMenu/friendMenu.jsx';
-import { ToastManager } from './components/common/toast/ToastManager.jsx';
-import { NotificationProvider } from './hooks/useNotification.jsx';
-import { ThemeProvider } from './hooks/useTheme.jsx';
-import { SidebarToggleProvider } from "./hooks/useSidebarToggleFriends.jsx"
-import { AuthProvider } from '../public/auth/AuthContext.jsx';
-import ScrollToTop from './hooks/ScrollToTop.jsx';
+import { ModalProvider } from "./components/common/modal/ModalContext.jsx";
+import ModalRenderer from "./components/common/modal/ModalRenderer.jsx";
+import { SidebarProvider } from "./hooks/useSidebarToggle.jsx";
+import { FriendMenu } from "./components/common/friendMenu/friendMenu.jsx";
+import { ToastManager } from "./components/common/toast/ToastManager.jsx";
+import { NotificationProvider } from "./hooks/useNotification.jsx";
+import { ThemeProvider } from "./hooks/useTheme.jsx";
+import { SidebarToggleProvider } from "./hooks/useSidebarToggleFriends.jsx";
+import { AuthProvider } from "../public/auth/AuthContext.jsx";
+import ScrollToTop from "./hooks/ScrollToTop.jsx";
 
-createRoot(document.getElementById('root')).render(
+import { Analytics } from "@vercel/analytics/react";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Analytics />
     <AuthProvider>
       <ThemeProvider>
         <Router>
@@ -50,16 +53,24 @@ createRoot(document.getElementById('root')).render(
                     <Route path="/you" element={<You />} />
                     <Route path="/register" element={<RegisterPage />} />
                     <Route path="/studio" element={<Studio />} />
-                    <Route path="/yourchannel/:url?" element={<YourChannel />} />
+                    <Route
+                      path="/yourchannel/:url?"
+                      element={<YourChannel />}
+                    />
                     <Route path="/shorts" element={<ShortsPage />} />
                     <Route path="/shorts/:id" element={<ShortsPage />} />
-                    <Route path='/watch/:id' element={<VideoPage />} />
-                    <Route path="/verification-successful" element={<SuccessPage />} />
+                    <Route path="/watch/:id" element={<VideoPage />} />
+                    <Route
+                      path="/verification-successful"
+                      element={<SuccessPage />}
+                    />
                     <Route path="/verification-error" element={<ErrorPage />} />
-                    <Route path="/reset-password" element={<ResetPasswordPage />} />
+                    <Route
+                      path="/reset-password"
+                      element={<ResetPasswordPage />}
+                    />
 
-
-                    <Route path='/Search' element={<Search />} />
+                    <Route path="/Search" element={<Search />} />
                   </Routes>
 
                   <ModalRenderer />
@@ -72,6 +83,5 @@ createRoot(document.getElementById('root')).render(
         </Router>
       </ThemeProvider>
     </AuthProvider>
-
-  </StrictMode>
+  </StrictMode>,
 );
